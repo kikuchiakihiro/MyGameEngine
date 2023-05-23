035c3d6ddec25d19a2a56e220a8139df5c992f05
明宏 菊池 <220039@jc-21.jp>
2023-05-23 10:47:28 +0900

    Windowsブラッシュアップ

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,10 @@
 //インクルード
 #include <Windows.h>
 
+const char* WIN_CLASS_NAME = "SampleGame";
+const char* TITLE_NAME = "サンプルゲーム";
+const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
 
 //プロトタイプ宣言
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
@@ -15,7 +19,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 	wc.cbSize = sizeof(WNDCLASSEX);             //この構造体のサイズ
 	wc.hInstance = hInstance;                   //インスタンスハンドル
-	wc.lpszClassName = "SampleGame";            //ウィンドウクラス名
+	wc.lpszClassName = WIN_CLASS_NAME;            //ウィンドウクラス名
 	wc.lpfnWndProc = WndProc;                   //ウィンドウプロシージャ
 	wc.style = CS_VREDRAW | CS_HREDRAW;         //スタイル（デフォルト）
 	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); //アイコン
@@ -29,15 +33,21 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
 
 	RegisterClassEx(&wc);  //クラスを登録
 
+	//ウィンドウサイズの計算
+	RECT winRect = { 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT };
+	AdjustWindowRect(&winRect, WS_OVERLAPPEDWINDOW, FALSE);
+	int winW = winRect.right - winRect.left;     //ウィンドウ幅
+	int winH = winRect.bottom - winRect.top;     //ウィンドウ高さ
+
   //ウィンドウを作成
 	HWND hWnd = CreateWindow(
-		"SampleGame",         //ウィンドウクラス名
-		"サンプルゲーム",     //タイトルバーに表示する内容
+		WIN_CLASS_NAME ,         //ウィンドウクラス名
+		TITLE_NAME,     //タイトルバーに表示する内容
 		WS_OVERLAPPEDWINDOW, //スタイル（普通のウィンドウ）
 		CW_USEDEFAULT,       //表示位置左（おまかせ）
 		CW_USEDEFAULT,       //表示位置上（おまかせ）
-		1200,                 //ウィンドウ幅
-		400,                 //ウィンドウ高さ
+		winW,                 //ウィンドウ幅
+		winH,                 //ウィンドウ高さ
 		NULL,                //親ウインドウ（なし）
 		NULL,                //メニュー（なし）
 		hInstance,           //インスタンス
@@ -51,7 +61,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, 
   //メッセージループ（何か起きるのを待つ）
 	MSG msg;
 	ZeroMemory(&msg, sizeof(msg));
-	while (msg.message = WM_QUIT)
+	while (msg.message != WM_QUIT)
 	{
 		//メッセージあり
 		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
