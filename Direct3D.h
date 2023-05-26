76c385350d1f57a48076277c2835a3ab257852f6
明宏 菊池 <220039@jc-21.jp>
2023-05-26 09:51:45 +0900

    a

---
+++ Direct3D.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <d3d11.h>
+
+#pragma comment(lib, "d3d11.lib")
+
+const int WINDOW_WIDTH = 800;  //ウィンドウの幅
+const int WINDOW_HEIGHT = 600; //ウィンドウの高さ
+namespace Direct3D
+
+{
+	
+	//初期化
+
+	void Initialize(int winW, int winH, HWND hWnd);
+
+
+
+	//描画開始
+
+	void BeginDraw();
+
+
+
+	//描画終了
+
+	void EndDraw();
+
+
+
+	//解放
+
+	void Release();
+
+};
+
