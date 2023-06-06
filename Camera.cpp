152da63bd175ab4f6eda83370c5a52499c463007
明宏 菊池 <220039@jc-21.jp>
2023-06-06 11:39:16 +0900

    a

--- Camera.cpp
+++ Camera.cpp
@@ -25,13 +25,13 @@ void Camera::Update()
 //位置を設定
 void Camera::SetPosition(XMVECTOR position)
 {
-	position = position_;
+	position_ = position;
 }
 
 //焦点を設定
 void Camera::SetTarget(XMVECTOR target)
 {
-	target = target_;
+	target_ = target;
 }
 
 //ビュー行列を取得
