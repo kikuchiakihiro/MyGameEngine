ba97ab36ec6c010ac2dd495c36b6c0583aa46ab7
明宏 菊池 <220039@jc-21.jp>
2023-06-13 11:07:52 +0900

    よ

--- Dice.cpp
+++ Dice.cpp
@@ -22,36 +22,35 @@ HRESULT Dice::Initialize()
 	   {XMVectorSet(1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f) },  // 前面（右下）2
 	   {XMVectorSet(-1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f) },  // 前面（左下）3
 
-	   //面４
-	   {XMVectorSet(-1.0f, 1.0f, 1.0f, 0.0f),XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f) },   // 測面左（左上）4
-	   {XMVectorSet(-1.0f,  1.0f, -1.0f, 0.0f),XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f) },  // 測面左（右上）5
-	   {XMVectorSet(-1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(1.0f, 0.5f, 0.0f, 0.0f) },  // 測面左（右下）6
-	   {XMVectorSet(-1.0f, -1.0f, 1.0f, 0.0f),XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f) },  // 測面左（左下）7
-
+	   //面２
+	   {XMVectorSet(-1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.25f, 0.0f, 0.0f, 0.0f) }, // 上面（左上）4
+	   {XMVectorSet(1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f) },  // 上面（右上）5
+	   {XMVectorSet(1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f) },    // 上面（右下）6
+	   {XMVectorSet(-1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f) },  // 上面（左下）7
+	  
 	   //面３
 	   {XMVectorSet(1.0f, 1.0f, -1.0f, 0.0f),XMVectorSet(0.5f, 0.0f, 0.0f, 0.0f) },    // 測面右（左上）8
 	   {XMVectorSet(1.0f,  1.0f, 1.0f, 0.0f),XMVectorSet(0.75f, 0.0f, 0.0f, 0.0f) },   // 測面右（右上）9
 	   {XMVectorSet(1.0f, -1.0f, 1.0f, 0.0f),XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f) },   // 測面右（右下）10
 	   {XMVectorSet(1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f) },   // 測面右（左下）11
 
-	   //面６
-	   {XMVectorSet(1.0f, 1.0f,   1.0f, 0.0f),XMVectorSet( 0.25f, 0.5f, 0.0f, 0.0f) },    // 背面（左上）12
-	   {XMVectorSet(-1.0f,  1.0f, 1.0f, 0.0f),XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f)   },   // 背面（右上）13
-	   {XMVectorSet(-1.0f, -1.0f, 1.0f, 0.0f),XMVectorSet(0.5f, 1.0f, 0.0f, 0.0f)   },   // 背面（右下）14
-	   {XMVectorSet(1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet( 0.25f, 1.0f, 0.0f, 0.0f) },   // 背面（左下）15
+	   //面４
+	  {XMVectorSet(-1.0f, 1.0f, 1.0f, 0.0f),XMVectorSet(0.75f,  0.0f, 0.0f, 0.0f) },   // 測面左（左上）12
+	  {XMVectorSet(-1.0f,  1.0f, -1.0f, 0.0f),XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f) },  // 測面左（右上）13
+	  {XMVectorSet(-1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(1.0f, 0.5f, 0.0f, 0.0f) },  // 測面左（右下）14
+	  {XMVectorSet(-1.0f, -1.0f, 1.0f, 0.0f),XMVectorSet(0.75f, 0.5f, 0.0f, 0.0f) },  // 測面左（左下）15
 
-	   //面５
-	   {XMVectorSet(-1.0f, 1.0f,  1.0f, 0.0f),XMVectorSet( 0.0f, 0.5f, 0.0f, 0.0f) },    // 上面（左上）16
+	  //面５
+	   {XMVectorSet(-1.0f, 1.0f,  1.0f, 0.0f),XMVectorSet(0.0f, 0.5f, 0.0f, 0.0f) },    // 上面（左上）16
 	   {XMVectorSet(1.0f,  1.0f,  1.0f, 0.0f),XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f) },   // 上面（右上）17
 	   {XMVectorSet(1.0f, 1.0f,  -1.0f, 0.0f),XMVectorSet(0.25f, 1.0f, 0.0f, 0.0f) },   // 上面（右下）18
-	   {XMVectorSet(-1.0f, 1.0f, -1.0f, 0.0f),XMVectorSet( 0.0f, 1.0f, 0.0f, 0.0f) },   // 上面（左下）19
+	   {XMVectorSet(-1.0f, 1.0f, -1.0f, 0.0f),XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f) },   // 上面（左下）19
 
-	   //面２
-	   {XMVectorSet(-1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet(0.25f, 0.0f, 0.0f, 0.0f) }, // 上面（左上）20
-	   {XMVectorSet( 1.0f, -1.0f, -1.0f, 0.0f),XMVectorSet( 0.5f, 0.0f, 0.0f, 0.0f) },  // 上面（右上）21
-	   {XMVectorSet( 1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet( 0.5f, 0.5f, 0.0f, 0.0f) },    // 上面（右下）22
-	   {XMVectorSet(-1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet(0.25f, 0.5f, 0.0f, 0.0f) },  // 上面（左下）23（左下）7
-
+	   //面６
+	   {XMVectorSet(1.0f, 1.0f,   1.0f, 0.0f),XMVectorSet( 0.25f, 0.5f, 0.0f, 0.0f) },    // 背面（左上）20
+	   {XMVectorSet(-1.0f,  1.0f, 1.0f, 0.0f),XMVectorSet(0.5f, 0.5f, 0.0f, 0.0f)   },   // 背面（右上）21
+	   {XMVectorSet(-1.0f, -1.0f, 1.0f, 0.0f),XMVectorSet(0.5f, 1.0f, 0.0f, 0.0f)   },   // 背面（右下）22
+	   {XMVectorSet(1.0f, -1.0f,  1.0f, 0.0f),XMVectorSet( 0.25f, 1.0f, 0.0f, 0.0f) },   // 背面（左下）23
 
 	};
 
@@ -71,12 +70,13 @@ HRESULT Dice::Initialize()
 		return E_FAIL;
 	}
 	//インデックス情報
-	int index[] = { 0, 2, 3,  0, 1, 2,     //面１
-					4, 6, 7,  4, 5, 6,     //面４
-					8,10,11,  8, 9,10,     //面３
-					12,14,15, 12,13,14,     //面６
-					16,18,19, 16,17,18,     //面５
-					20,22,23, 20,21,22,     };
+	int index[] = { 0, 2, 3,  0, 1, 2,     
+					4, 6, 7,  4, 5, 6,     
+					8,10,11,  8, 9,10,     
+					12,14,15, 12,13,14,    
+					16,18,19, 16,17,18,    
+					20,22,23, 20,21,22,        
+	};
 
 	D3D11_BUFFER_DESC   bd;
 	bd.Usage = D3D11_USAGE_DEFAULT;
