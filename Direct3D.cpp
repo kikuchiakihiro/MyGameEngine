a2445b3f4fdb965ef7bc7e2b4577af51bf9ffe6b
明宏 菊池 <220039@jc-21.jp>
2023-06-14 09:02:47 +0900

    a

--- Direct3D.cpp
+++ Direct3D.cpp
@@ -108,14 +108,14 @@ void Direct3D::InitShader()
 	assert(pCompileVS != nullptr);
 	pDevice_->CreateVertexShader(pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), NULL, &pVertexShader_);
 	pCompileVS->Release();
-
+	
 	D3D11_INPUT_ELEMENT_DESC layout[] = {
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,  D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, sizeof(DirectX::XMVECTOR) , D3D11_INPUT_PER_VERTEX_DATA, 0 },//UVÀ•W//ˆÊ’u
 		{ "NORMAL",	0, DXGI_FORMAT_R32G32B32_FLOAT, 0, sizeof(XMVECTOR) * 2 ,	D3D11_INPUT_PER_VERTEX_DATA, 0 },//–@ü
 	};
-	pDevice_->CreateInputLayout(layout, 3, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout_);
-
+	
+	 pDevice_->CreateInputLayout(layout, 3, pCompileVS->GetBufferPointer(), pCompileVS->GetBufferSize(), &pVertexLayout_);
 	
 
 	ID3DBlob* pCompilePS = nullptr;
