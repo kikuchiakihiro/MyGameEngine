31faca5a394c754e81ba7b7be3ce99fc6ea75259
明宏 菊池 <220039@jc-21.jp>
2023-06-16 12:25:50 +0900

    ????

--- Sprite.cpp
+++ Sprite.cpp
@@ -18,8 +18,7 @@ HRESULT Sprite::Initialize()
 		{ XMVectorSet(-1.0f,  1.0f, 0.0f, 0.0f),XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f) },   // 四角形の頂点（左上）
 		{ XMVectorSet(1.0f,  1.0f, 0.0f, 0.0f),	XMVectorSet(1.0f, 0.0f, 0.0f, 0.0f)  },   // 四角形の頂点（右上）
 		{ XMVectorSet(1.0f, -1.0f, 0.0f, 0.0f),	XMVectorSet(1.0f, 1.0f, 0.0f, 0.0f)},   // 四角形の頂点（右下）
-		{ XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f),XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f) },   // 四角形の頂点（左下）
-			
+		{ XMVectorSet(-1.0f, -1.0f, 0.0f, 0.0f),XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f) },   // 四角形の頂点（左下）	
 	};
 
 	 //頂点データ用バッファの設定
@@ -38,7 +37,7 @@ HRESULT Sprite::Initialize()
 		return E_FAIL;
 	}
 	//インデックス情報
-	int index[] = { 0,1,2, 0,2,3, /*0,3,5 ,3,4,5*/ };
+	int index[] = { 0,1,2, 0,2,3, };
 
 	D3D11_BUFFER_DESC   bd;
 	bd.Usage = D3D11_USAGE_DEFAULT;
@@ -80,29 +79,14 @@ HRESULT Sprite::Initialize()
 
 void Sprite::Draw(XMMATRIX& worldMatrix)
 {
-	////コンスタントバッファに渡す情報
-	//XMVECTOR position = { 0, 3, -10, 0 };	//カメラの位置
-	//XMVECTOR target = { 0, 0, 0, 0 };	//カメラの焦点
-	//XMMATRIX view = XMMatrixLookAtLH(position, target, XMVectorSet(0, 1, 0, 0));	//ビュー行列
-	//XMMATRIX proj = XMMatrixPerspectiveFovLH(XM_PIDIV4, 800.0f / 600.0f, 0.1f, 100.0f);//射影行列 画角　XM_PIDIV4は45度, アスペクト比,　ニアクリッピング面,　ファークリッピング面
-	//D3D11_MAPPED_SUBRESOURCE pdata;
-	CONSTANT_BUFFER cb;
 	
+	CONSTANT_BUFFER cb;
 	cb.matW = XMMatrixTranspose(worldMatrix);
-	//Direct3D::pContext_->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	// GPUからのデータアクセスを止める
-	//memcpy_s(pdata.pData, pdata.RowPitch, (void*)(&cb), sizeof(cb));	// データを値を送る
-
 	ID3D11SamplerState* pSampler = pTexture_->GetSampler();
-
 	Direct3D::pContext_->PSSetSamplers(0, 1, &pSampler);
 
-
-
 	ID3D11ShaderResourceView* pSRV = pTexture_->GetSRV();
-
 	Direct3D::pContext_->PSSetShaderResources(0, 1, &pSRV);
-
-
 	Direct3D::pContext_->Unmap(pConstantBuffer_, 0);	//再開
 
 	//頂点バッファ
@@ -137,68 +121,40 @@ void Sprite::Release()
 	SAFE_RELEASE(pVertexBuffer_);
 }
 
-void Sprite::InitVertexData()
-{
-}
-
-HRESULT Sprite::CreateVertexBuffer()
-{
-	return E_NOTIMPL;
-}
-
-void Sprite::InitIndexData()
-{
-}
-
-HRESULT Sprite::CreateIndexBuffer()
-{
-	return E_NOTIMPL;
-}
-
-HRESULT Sprite::CreateConstantBuffer()
-{
-	return E_NOTIMPL;
-}
-
-HRESULT Sprite::LoadTexture()
-{
-	pTexture_ = new Texture;
-	pTexture_->Load("Assets\\MicrosoftTeams-image.png");
-
-}
-
-void Sprite::PassDataToCB(DirectX::XMMATRIX& worldMatrix)
-{
-	CONSTANT_BUFFER cb;
-	cb.matW = XMMatrixTranspose(worldMatrix);
-	ID3D11SamplerState* pSampler = pTexture_->GetSampler();
-	Direct3D::pContext_->PSSetSamplers(0, 1, &pSampler);
-
-
-
-	ID3D11ShaderResourceView* pSRV = pTexture_->GetSRV();
-	Direct3D::pContext_->PSSetShaderResources(0, 1, &pSRV);
-	Direct3D::pContext_->Unmap(pConstantBuffer_, 0);	//再開
-
-	//頂点バッファ
-	UINT stride = sizeof(VERTEX);
-	UINT offset = 0;
-	Direct3D::pContext_->IASetVertexBuffers(0, 1, &pVertexBuffer_, &stride, &offset);
-
-	//インデックスバッファーをセット
-	stride = sizeof(int);
-	offset = 0;
-	Direct3D::pContext_->IASetIndexBuffer(pIndexBuffer_, DXGI_FORMAT_R32_UINT, 0);
-
-	//コンスタントバッファ
-	Direct3D::pContext_->VSSetConstantBuffers(0, 1, &pConstantBuffer_);	//頂点シェーダー用	
-	Direct3D::pContext_->PSSetConstantBuffers(0, 1, &pConstantBuffer_);	//ピクセルシェーダー用
-
-	Direct3D::pContext_->DrawIndexed(6, 0, 0);
-
-}
-
-void Sprite::SetBufferToPipeline()
-{
-}
+//void Sprite::InitVertexData()
+//{
+//}
+//
+//HRESULT Sprite::CreateVertexBuffer()
+//{
+//	return E_NOTIMPL;
+//}
+//
+//void Sprite::InitIndexData()
+//{
+//}
+//
+//HRESULT Sprite::CreateIndexBuffer()
+//{
+//	return E_NOTIMPL;
+//}
+//
+//HRESULT Sprite::CreateConstantBuffer()
+//{
+//	return E_NOTIMPL;
+//}
+//
+//HRESULT Sprite::LoadTexture()
+//{
+//	return E_NOTIMPL;
+//}
+//
+//
+//void Sprite::PassDataToCB(DirectX::XMMATRIX& worldMatrix)
+//{
+//}
+//
+//void Sprite::SetBufferToPipeline()
+//{
+//}
 
