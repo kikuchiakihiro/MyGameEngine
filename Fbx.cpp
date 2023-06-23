7ea3fdf3ea24267c0d6b9117da842efc80556aa2
明宏 菊池 <220039@jc-21.jp>
2023-06-23 11:35:15 +0900

    だよね

--- Fbx.cpp
+++ Fbx.cpp
@@ -1,60 +1,231 @@
-//#include "Fbx.h"
-//
-//Fbx::Fbx():
-//	pVertexBuffer_(nullptr), pIndexBuffer_(nullptr),pConstantBuffer_(nullptr)
-//	, vertexCount_(0), polygonCount_(0)
-//{
-//}
-//
-//Fbx::~Fbx()
-//{
-//	Release();
-//}
-//
-//HRESULT Fbx::Load(std::string fileName)
-//{
-//	//マネージャを生成
-//	FbxManager* pFbxManager = FbxManager::Create();
-//
-//	//インポーターを生成
-//	FbxImporter* fbxImporter = FbxImporter::Create(pFbxManager, "imp");
-//	fbxImporter->Initialize(fileName.c_str(), -1, pFbxManager->GetIOSettings());
-//
-//	//シーンオブジェクトにFBXファイルの情報を流し込む
-//	FbxScene* pFbxScene = FbxScene::Create(pFbxManager, "fbxscene");
-//	fbxImporter->Import(pFbxScene);
-//	fbxImporter->Destroy();
-//
-//	//メッシュ情報を取得
-//	FbxNode* rootNode = pFbxScene->GetRootNode();
-//	FbxNode* pNode = rootNode->GetChild(0);
-//	FbxMesh* mesh = pNode->GetMesh();
-//
-//	//各情報の個数を取得
-//
-//	vertexCount_ = mesh->GetControlPointsCount();	//頂点の数
-//
-//	polygonCount_ = mesh->GetPolygonCount();	//ポリゴンの数
-//
-//	InitVertex(mesh);		//頂点バッファ準備
-//
-//	//マネージャ解放
-//	pFbxManager->Destroy();
-//	return S_OK;
-//}
-//
-//void Fbx::InitVertex(fbxsdk::FbxMesh* mesh)
+#include "Fbx.h"
+#include "Camera.h"
+Fbx::Fbx():
+	pVertexBuffer_(nullptr), pIndexBuffer_(nullptr),pConstantBuffer_(nullptr)
+	, vertexCount_(0), polygonCount_(0)
+{
+}
+
+Fbx::~Fbx()
+{
+	Release();
+}
+
+HRESULT Fbx::Load(std::string fileName)
+{
+	//マネージャを生成
+	FbxManager* pFbxManager = FbxManager::Create();
+
+	//インポーターを生成
+	FbxImporter* fbxImporter = FbxImporter::Create(pFbxManager, "imp");
+	fbxImporter->Initialize(fileName.c_str(), -1, pFbxManager->GetIOSettings());
+
+	//シーンオブジェクトにFBXファイルの情報を流し込む
+	FbxScene* pFbxScene = FbxScene::Create(pFbxManager, "fbxscene");
+	fbxImporter->Import(pFbxScene);
+	fbxImporter->Destroy();
+
+	//メッシュ情報を取得
+	FbxNode* rootNode = pFbxScene->GetRootNode();
+	FbxNode* pNode = rootNode->GetChild(0);
+	FbxMesh* mesh = pNode->GetMesh();
+
+	//各情報の個数を取得
+
+	vertexCount_ = mesh->GetControlPointsCount();	//頂点の数
+
+	polygonCount_ = mesh->GetPolygonCount();	//ポリゴンの数
+
+	InitVertex(mesh);		//頂点バッファ準備
+	InitIndex(mesh);		//インデックスバッファ準備
+	InitConstant(mesh);     //コンスタントバッファ準備
+	//マネージャ解放
+	pFbxManager->Destroy();
+	return S_OK;
+}
+
+HRESULT Fbx::InitVertex(fbxsdk::FbxMesh* mesh)
+{
+	//頂点情報を入れる配列
+	VERTEX* vertices = new VERTEX[vertexCount_];
+
+	//全ポリゴン
+	for (DWORD poly = 0; poly < polygonCount_; poly++)
+	{
+		//3頂点分
+		for (int vertex = 0; vertex < 3; vertex++)
+		{
+			//調べる頂点の番号
+			int index = mesh->GetPolygonVertex(poly, vertex);
+
+			//頂点の位置
+			FbxVector4 pos = mesh->GetControlPointAt(index);
+			vertices[index].position = XMVectorSet((float)pos[0], (float)pos[1], (float)pos[2], 0.0f);
+		}
+	}
+
+	HRESULT hr;
+	D3D11_BUFFER_DESC bd_vertex;
+	bd_vertex.ByteWidth = sizeof(VERTEX) * vertexCount_;
+	bd_vertex.Usage = D3D11_USAGE_DEFAULT;
+	bd_vertex.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd_vertex.CPUAccessFlags = 0;
+	bd_vertex.MiscFlags = 0;
+	bd_vertex.StructureByteStride = 0;
+	D3D11_SUBRESOURCE_DATA data_vertex;
+	data_vertex.pSysMem = vertices;
+	hr = Direct3D::pDevice_->CreateBuffer(&bd_vertex, &data_vertex, &pVertexBuffer_);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, "頂点バッファの作成に失敗しました", "エラー", MB_OK);
+		return hr;
+		
+	}
+	return S_OK;
+}
+
+//インデックスバッファ準備
+HRESULT Fbx::InitIndex(fbxsdk::FbxMesh* mesh)
+{
+	int* index = new int[polygonCount_ * 3];
+	int count = 0;
+
+	//全ポリゴン
+	for (DWORD poly = 0; poly < polygonCount_; poly++)
+	{
+		//3頂点分
+		for (DWORD vertex = 0; vertex < 3; vertex++)
+		{
+			index[count] = mesh->GetPolygonVertex(poly, vertex);
+			count++;
+		}
+	}
+
+	D3D11_BUFFER_DESC   bd;
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(int) * index[count];
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	bd.MiscFlags = 0;
+
+	D3D11_SUBRESOURCE_DATA InitData;
+	InitData.pSysMem = index;
+	InitData.SysMemPitch = 0;
+	InitData.SysMemSlicePitch = 0;
+
+	HRESULT hr;
+	hr = Direct3D::pDevice_->CreateBuffer(&bd, &InitData, &pIndexBuffer_);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, "インデックスバッファの作成に失敗しました", "エラー", MB_OK);
+		return hr;
+	}
+	return S_OK;
+}
+
+HRESULT Fbx::InitConstant(fbxsdk::FbxMesh* mesh)
+{
+	D3D11_BUFFER_DESC cb;
+	cb.ByteWidth = sizeof(CONSTANT_BUFFER);
+	cb.Usage = D3D11_USAGE_DYNAMIC;
+	cb.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cb.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	cb.MiscFlags = 0;
+	cb.StructureByteStride = 0;
+
+	// コンスタントバッファの作成
+	HRESULT hr;
+	hr = Direct3D::pDevice_->CreateBuffer(&cb, nullptr, &pConstantBuffer_);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, "コンスタントバッファの作成に失敗しました", "エラー", MB_OK);
+		return hr;
+	}
+	return S_OK;
+}
+
+
+void Fbx::Draw(Transform& transform)
+{
+
+	CONSTANT_BUFFER cb;
+	cb.matWVP = XMMatrixTranspose(transform.GetWorldMatrix() * Camera::GetViewMatrix() * Camera::GetProjectionMatrix());
+	cb.matNormal = XMMatrixTranspose(transform.GetNormalMatrix());
+
+	D3D11_MAPPED_SUBRESOURCE pdata;
+	Direct3D::pContext_->Map(pConstantBuffer_, 0, D3D11_MAP_WRITE_DISCARD, 0, &pdata);	// GPUからのデータアクセスを止める
+	memcpy_s(pdata.pData, pdata.RowPitch, (void*)(&cb), sizeof(cb));	// データを値を送る
+
+	ID3D11SamplerState* pSampler = pTexture_->GetSampler();
+
+	Direct3D::pContext_->PSSetSamplers(0, 1, &pSampler);
+
+	ID3D11ShaderResourceView* pSRV = pTexture_->GetSRV();
+
+	Direct3D::pContext_->PSSetShaderResources(0, 1, &pSRV);
+
+	Direct3D::pContext_->Unmap(pConstantBuffer_, 0);	//再開
+
+	UINT stride = sizeof(VERTEX);
+	UINT offset = 0;
+	Direct3D::pContext_->IASetVertexBuffers(0, 1, &pVertexBuffer_, &stride, &offset);
+
+	// インデックスバッファーをセット
+	stride = sizeof(int);
+	offset = 0;
+	Direct3D::pContext_->IASetIndexBuffer(pIndexBuffer_, DXGI_FORMAT_R32_UINT, 0);
+
+	//コンスタントバッファ
+	Direct3D::pContext_->VSSetConstantBuffers(0, 1, &pConstantBuffer_);	//頂点シェーダー用	
+	Direct3D::pContext_->PSSetConstantBuffers(0, 1, &pConstantBuffer_);	//ピクセルシェーダー用
+
+
+	Direct3D::SetShader(SHADER_3D);
+	transform.Calclation();//トランスフォームを計算
+	////コンスタントバッファに情報を渡す
+	//PassDataToCB(transform);
+
+	////頂点バッファ、インデックスバッファ、コンスタントバッファをパイプラインにセット
+	//SetBufferToPipeline();
+
+	//描画
+	Direct3D::pContext_->DrawIndexed(, 0, 0);
+
+}
+
+void Fbx::Release()
+{
+	SAFE_RELEASE(pConstantBuffer_);
+	SAFE_RELEASE(pIndexBuffer_);
+	SAFE_RELEASE(pVertexBuffer_);
+}
+
+HRESULT Fbx::LoadTexture()
+{
+	pTexture_ = new Texture;
+
+	HRESULT hr;
+	hr = pTexture_->Load("Assets\\oden.png");
+	if (FAILED(hr))
+	{
+		MessageBox(NULL, "テクスチャの作成に失敗しました", "エラー", MB_OK);
+		return hr;
+	}
+	return S_OK;
+}
+
+//void Fbx::PassDataToCB(Transform transform)
 //{
 //	
 //}
 //
-//void Fbx::Draw(Transform& transform)
+//void Fbx::SetBufferToPipeline()
 //{
-//}
 //
-//void Fbx::Release()
-//{
-//	SAFE_RELEASE(pConstantBuffer_);
-//	SAFE_RELEASE(pIndexBuffer_);
-//	SAFE_RELEASE(pVertexBuffer_);
+//	
+//
 //}
+
+
+
+
--- Fbx.h
+++ Fbx.h
@@ -1,42 +1,52 @@
-//#pragma once
-//
-//#include <d3d11.h>
-//#include <fbxsdk.h>
-//#include <string>
-//#include "Transform.h"
-//#include "Direct3D.h"
-//
-//#pragma comment(lib, "LibFbxSDK-Md.lib")
-//#pragma comment(lib, "LibXml2-Md.lib")
-//#pragma comment(lib, "zlib-Md.lib")
-//
-//class Fbx
-//{
-//	struct CONSTANT_BUFFER
-//	{
-//		XMMATRIX	matWVP;
-//		XMMATRIX	matNormal;
-//	};
-//
-//	struct VERTEX
-//	{
-//		XMFLOAT3 position;
-//	};
-//
-//	int vertexCount_;	//頂点数
-//	int polygonCount_;	//ポリゴン数
-//
-//
-//	ID3D11Buffer* pVertexBuffer_;
-//	ID3D11Buffer* pIndexBuffer_;
-//	ID3D11Buffer* pConstantBuffer_;
-//
-//public:
-//
-//	Fbx();
-//	~Fbx();
-//	HRESULT Load(std::string fileName);
-//	void InitVertex(fbxsdk::FbxMesh* mesh);
-//	void    Draw(Transform& transform);
-//	void    Release();
-//};
+#pragma once
+
+#include <d3d11.h>
+#include <fbxsdk.h>
+#include <string>
+#include "Transform.h"
+#include "Direct3D.h"
+#include "Texture.h"
+
+#pragma comment(lib, "LibFbxSDK-Md.lib")
+#pragma comment(lib, "LibXml2-Md.lib")
+#pragma comment(lib, "zlib-Md.lib")
+
+class Fbx
+{
+	struct CONSTANT_BUFFER
+	{
+		XMMATRIX	matWVP;
+		XMMATRIX	matNormal;
+	};
+
+	struct VERTEX
+	{
+		XMVECTOR position;
+	};
+
+	int vertexCount_;	//頂点数
+	int polygonCount_;	//ポリゴン数
+
+
+	ID3D11Buffer* pVertexBuffer_;
+	ID3D11Buffer* pIndexBuffer_;
+	ID3D11Buffer* pConstantBuffer_;
+	Texture* pTexture_;
+
+public:
+
+	Fbx();
+	~Fbx();
+	HRESULT Load(std::string fileName);
+	HRESULT InitVertex(fbxsdk::FbxMesh* mesh);
+	HRESULT InitIndex(fbxsdk::FbxMesh* mesh);
+	HRESULT InitConstant(fbxsdk::FbxMesh* mesh);
+	void    Draw(Transform& transform);
+	void    Release();
+
+	HRESULT LoadTexture();
+
+private:
+	//void PassDataToCB(Transform transform);	//コンスタントバッファに各種情報を渡す
+	//void SetBufferToPipeline();
+};
