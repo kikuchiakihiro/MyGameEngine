9872abf4d2b1def1b5d195b20810fedb4963b623
明宏 菊池 <220039@jc-21.jp>
2023-08-25 11:07:55 +0900

    Box

--- TestScene.cpp
+++ TestScene.cpp
@@ -1,7 +1,7 @@
 #include "TestScene.h"
 #include "Engine/Input.h"
 #include "Engine/SceneManager.h"
-
+#include "Stage.h"
 
 TestScene::TestScene(GameObject* parent):GameObject(parent,"TestScene")
 {
@@ -10,15 +10,12 @@ TestScene::TestScene(GameObject* parent):GameObject(parent,"TestScene")
 
 void TestScene::Initialize()
 {
+	Instantiate<Stage>(this);
 }
 
 void TestScene::Update()
 {
-	//GameObject* dbgTest = FindObject("SceneManager");
-	/*if (Input::IsKey(DIK_RETURN)) {
-		SceneManager* pSceneManager = (SceneManager *)FindObject("SceneManager");
-		pSceneManager->ChangeScene(SCENE_ID_PLAY);
-	}*/
+	
 }
 
 void TestScene::Draw()
